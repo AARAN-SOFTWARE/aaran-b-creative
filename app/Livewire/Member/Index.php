9a297b963a2d80591ddb63175d889b9b680bcfbb
Divya <divyadharshini146@gmail.com>
2024-10-07 18:39:53 +0530

    #12 working on tree net-work

--- app/Livewire/Member/Index.php
+++ app/Livewire/Member/Index.php
@@ -4,65 +4,16 @@ namespace App\Livewire\Member;
 
 use Aaran\Member\Models\Member;
 use App\Livewire\Trait\CommonTraitNew;
+use App\Models\User;
 use Livewire\Component;
 
 class Index extends Component
 {
-    use CommonTraitNew;
 
-    public $level;
-    public $uid;
-    public $member_id;
-
-
-    public function getSave()
-    {
-        if ($this->common->vname != '') {
-            if ($this->common->vid == '') {
-                $Member = new Member();
-                $extraFields = [
-                    'level' => $this->level,
-                ];
-                $this->common->save($Member, $extraFields);
-                $message = "Saved";
-            }
-            $this->dispatch('notify', ...['type' => 'success', 'content' => $message . ' Successfully']);
-        }
-        $this->clearFields();
-
-
-    }
-
-    #region[Get-Obj]
-    public function getObj($id)
-    {
-        if ($id) {
-            $Member = Member::find($id);
-            $this->common->vid = $Member->id;
-            $this->common->vname = $Member->vname;
-            $this->level = $Member->level;
-            $this->common->active_id = $Member->active_id;
-            return $Member;
-        }
-        return null;
-    }
-    #endregion
-
-
-    #region[Clear-Fields]
-    public function clearFields(): void
-    {
-        $this->common->vid = '';
-        $this->common->vname = '';
-        $this->level = '';
-        $this->common->active_id = 1;
-    }
-
-    #endregion
     public function render()
     {
         return view('livewire.member.index')->with([
-            'list' => $this->getListForm->getList( Member::class),
+            'list' => User::all(),
         ]);
     }
 }
--- resources/views/livewire/member/index.blade.php
+++ resources/views/livewire/member/index.blade.php
@@ -1,6 +1,51 @@
 <div>
     <x-slot name="header">Members</x-slot>
     <div>
-        This is a Members Page
+        <x-forms.m-panel>
+
+            <x-forms.top-controls/>
+
+            <x-table.caption :caption="'Demo Request'">
+                {{$list->count()}}
+            </x-table.caption>
+
+            <x-table.form>
+                <x-slot:table_header name="table_header" class="bg-purple-100">
+
+                    <x-table.header-serial></x-table.header-serial>
+
+                    <x-table.header-text>profile_photo_path</x-table.header-text>
+                    <x-table.header-text wire:click.prevent="sortBy('id')">username
+                    </x-table.header-text>
+                    <x-table.header-text wire:click.prevent="sortBy('id')">name
+                    </x-table.header-text>
+                    <x-table.header-text wire:click.prevent="sortBy('id')">email
+                    </x-table.header-text>
+                    <x-table.header-text>phone</x-table.header-text>
+                    <x-table.header-text>usertype</x-table.header-text>
+
+
+                </x-slot:table_header>
+
+                <x-slot:table_body name="table_body">
+
+                    @foreach($list as $index=>$row)
+
+                        <x-table.row>
+                            <x-table.cell-text>{{$index+1}}</x-table.cell-text>
+                            <x-table.cell-text>{{$row->profile_photo_path}}</x-table.cell-text>
+                            <x-table.cell-text>{{$row->username}}</x-table.cell-text>
+                            <x-table.cell-text>{{$row->name}}</x-table.cell-text>
+                            <x-table.cell-text>{{$row->email}}</x-table.cell-text>
+                            <x-table.cell-text>{{$row->phone}}</x-table.cell-text>
+                            <x-table.cell-text>{{$row->usertype}}</x-table.cell-text>
+                        </x-table.row>
+                    @endforeach
+                </x-slot:table_body>
+
+            </x-table.form>
+            <x-modal.delete/>
+        </x-forms.m-panel>
+
     </div>
 </div>
