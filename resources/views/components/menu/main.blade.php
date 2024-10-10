a3cb671949c3669c9498814be21d632880b039d5
azhagiri <azhagirishankar5@gmail.com>
2024-10-10 18:50:57 +0530

    #14 home page

---
+++ resources/views/components/menu/main.blade.php
@@ -0,0 +1,85 @@
+<div class="fixed z-50 w-full h-40 bg-white flex-col flex">
+    <div class="bg-blue-900 h-1"></div>
+    {{-- <div class="w-2/3 self-end h-16 bg-blue-900 transform "> --}}
+
+    <div class="self-end w-2/3 h-12 bg-blue-900 flex justify-center gap-5 items-center [clip-path:polygon(0%_0%,_100%_0%,_100%_100%,_5%_100%)]">
+        <div class="flex flex-row justify-center items-center gap-x-5 text-white text-sm font-semibold">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-4 text-white">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M11.48 3.499a.562.562 0 0 1 1.04 0l2.125 5.111a.563.563 0 0 0 .475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 0 0-.182.557l1.285 5.385a.562.562 0 0 1-.84.61l-4.725-2.885a.562.562 0 0 0-.586 0L6.982 20.54a.562.562 0 0 1-.84-.61l1.285-5.386a.562.562 0 0 0-.182-.557l-4.204-3.602a.562.562 0 0 1 .321-.988l5.518-.442a.563.563 0 0 0 .475-.345L11.48 3.5Z" />
+            </svg>
+            <span>Welcome to the Aaran business </span>
+            <span class="inline-flex items-center gap-3"><svg width="" height="" viewBox="0 0 90 90" fill="none" xmlns="http://www.w3.org/2000/svg" class="w-4 h-4">
+                    <path d="M85 45C85 22.9086 67.0914 5 45 5C22.9086 5 5 22.9086 5 45C5 64.9649 19.6273 81.5133 38.75 84.5141V56.5625H28.5938V45H38.75V36.1875C38.75 26.1625 44.7219 20.625 53.8586 20.625C58.2352 20.625 62.8125 21.4063 62.8125 21.4063V31.25H57.7687C52.8 31.25 51.25 34.3336 51.25 37.4969V45H62.3438L60.5703 56.5625H51.25V84.5141C70.3727 81.5133 85 64.9656 85 45Z" fill="white" />
+                    <path d="M60.5703 56.5625L62.3438 45H51.25V37.4969C51.25 34.3336 52.8 31.25 57.7687 31.25H62.8125V21.4062C62.8125 21.4062 58.2352 20.625 53.8586 20.625C44.7219 20.625 38.75 26.1625 38.75 36.1875V45H28.5938V56.5625H38.75V84.5141C40.7867 84.8336 42.8734 85 45 85C47.1266 85 49.2133 84.8336 51.25 84.5141V56.5625H60.5703Z" fill="" />
+                </svg>
+                <svg width="" height="" viewBox="0 0 90 90" fill="none" xmlns="http://www.w3.org/2000/svg" class="w-4 h-4">
+
+                    <path d="M83.3282 25.8014C82.4082 22.3373 79.6977 19.6086 76.2555 18.6827C70.0168 17 45 17 45 17C45 17 19.9832 17 13.7441 18.6827C10.3023 19.6086 7.59136 22.3373 6.67136 25.8014C5 32.0809 5 45.1818 5 45.1818C5 45.1818 5 58.2832 6.67136 64.5627C7.59136 68.0268 10.3023 70.755 13.7441 71.6813C19.9832 73.3636 45 73.3636 45 73.3636C45 73.3636 70.0168 73.3636 76.2555 71.6813C79.6977 70.755 82.4082 68.0268 83.3282 64.5627C85 58.2832 85 45.1818 85 45.1818C85 45.1818 85 32.0809 83.3282 25.8014Z" fill="white" />
+                    <path d="M36.8125 57.0766L57.7216 45.1821L36.8125 33.2871V57.0766Z" fill="navy" />
+                </svg>
+            </span>
+
+
+        </div>
+    </div>
+    <div class="w-full h-16 flex justify-center items-center">
+        <div class="w-8/12 flex flex-row justify-between items-center">
+
+            <div class="4/12 inline-flex items-center justify-center gap-x-5 font-semibold text-gray-500">
+                <span><img src="../../../../images/network/logo/abc.png" alt="" class="w-40 h-auto"></span>
+            </div>
+
+            <div class="w-8/12 flex flex-row justify-evenly ">
+                <div class="inline-flex items-center gap-x-5 font-bold">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-6 fill-red-700">
+                        <path fill-rule="evenodd" d="M1.5 4.5a3 3 0 0 1 3-3h1.372c.86 0 1.61.586 1.819 1.42l1.105 4.423a1.875 1.875 0 0 1-.694 1.955l-1.293.97c-.135.101-.164.249-.126.352a11.285 11.285 0 0 0 6.697 6.697c.103.038.25.009.352-.126l.97-1.293a1.875 1.875 0 0 1 1.955-.694l4.423 1.105c.834.209 1.42.959 1.42 1.82V19.5a3 3 0 0 1-3 3h-2.25C8.552 22.5 1.5 15.448 1.5 6.75V4.5Z" clip-rule="evenodd" />
+                    </svg>
+                    <div>
+                        <div class="text-sm capitalize">Call us</div>
+                        <div class="text-sm capitalize text-gray-600">9876543210</div>
+
+                    </div>
+                </div>
+                <div class="inline-flex items-center gap-x-5 font-bold">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-6 fill-red-700">
+                        <path fill-rule="evenodd" d="M1.5 4.5a3 3 0 0 1 3-3h1.372c.86 0 1.61.586 1.819 1.42l1.105 4.423a1.875 1.875 0 0 1-.694 1.955l-1.293.97c-.135.101-.164.249-.126.352a11.285 11.285 0 0 0 6.697 6.697c.103.038.25.009.352-.126l.97-1.293a1.875 1.875 0 0 1 1.955-.694l4.423 1.105c.834.209 1.42.959 1.42 1.82V19.5a3 3 0 0 1-3 3h-2.25C8.552 22.5 1.5 15.448 1.5 6.75V4.5Z" clip-rule="evenodd" />
+                    </svg>
+                    <div>
+                        <div class="text-sm capitalize">Call us</div>
+                        <div class="text-sm capitalize text-gray-600">9876543210</div>
+
+                    </div>
+                </div>
+
+                <button class="py-2 px-8 bg-red-500 text-white rounded-br-2xl">
+                    Join now
+                </button>
+            </div>
+        </div>
+    </div>
+    <div class="w-full absolute bottom-0 bg-red-500 h-1"></div>
+
+    <div class="absolute -bottom-6 self-end w-2/3 h-16 bg-red-500 flex justify-center [clip-path:polygon(5%_0%,_100%_0%,_100%_100%,_0%_100%)]">
+        <div class="w-10/12 mx-auto flex flex-row justify-evenly items-center text-sm text-white font-bold">
+            <div>HOME</div>
+            <div>ABOUT</div>
+            <div>OUR PRODUCTS</div>
+            <div>CONTACT</div>
+            <div class="inline-flex items-center justify-center gap-x-2">
+                <span>LOGIN</span>
+                <span>
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-5 fill-white">
+                        <path fill-rule="evenodd" d="M7.5 6v.75H5.513c-.96 0-1.764.724-1.865 1.679l-1.263 12A1.875 1.875 0 0 0 4.25 22.5h15.5a1.875 1.875 0 0 0 1.865-2.071l-1.263-12a1.875 1.875 0 0 0-1.865-1.679H16.5V6a4.5 4.5 0 1 0-9 0ZM12 3a3 3 0 0 0-3 3v.75h6V6a3 3 0 0 0-3-3Zm-3 8.25a3 3 0 1 0 6 0v-.75a.75.75 0 0 1 1.5 0v.75a4.5 4.5 0 1 1-9 0v-.75a.75.75 0 0 1 1.5 0v.75Z" clip-rule="evenodd" />
+                    </svg>
+
+
+                </span>
+            </div>
+
+        </div>
+    </div>
+
+
+
+</div>
+</div>
