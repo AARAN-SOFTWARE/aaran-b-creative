56644780260d7c7451dee4bb96a58b04342e5791
azhagiri <azhagirishankar5@gmail.com>
2024-10-18 19:24:31 +0530

    #17 working on homepage

--- resources/views/livewire/web/home/index.blade.php
+++ resources/views/livewire/web/home/index.blade.php
@@ -1,6 +1,8 @@
 <div class="scroll-smooth">
 {{--    <x-slider.home/>--}}
     <x-slider.new/>
+    <x-web.home.milestone />
+    <x-web.home.service />
 
     <x-web.home.footer-address/>
     <x-web.home.copyright/>
