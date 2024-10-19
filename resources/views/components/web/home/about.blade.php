19759b36df2123b63729ea473b43ce14e46ee67c
azhagiri <azhagirishankar5@gmail.com>
2024-10-19 19:05:11 +0530

    #18 working on homepage

---
+++ resources/views/components/web/home/about.blade.php
@@ -0,0 +1,34 @@
+<div class="w-10/12 h-screen mx-auto py-16 flex font-lex ">
+    <div class="flex bg-red-50 rounded-md">
+    <div class="w-1/2 relative ">
+        <img src="../../../../images/aboutoffice.png" alt=""
+             class="absolute z-[1]  animate__animated wow animate__bounceInDown"
+             data-wow-duration="3s">
+    </div>
+    <div
+        class="w-1/2 h-full  [clip-path:polygon(20%_0%,_100%_0%,_100%_100%,_0%_100%)] p-5  bg-cover text-white flex-col flex justify-center rounded-r-md"
+        style="background-image: url('../../../images/bg-7.jpg');">
+        <div class="w-3/4 self-end flex-col flex justify-center space-y-5 p-5 rounded-md">
+            <div class=" text-6xl text-yellow-50 font-semibold text-start underline">About us</div>
+            <div class="  font-semibold text-md leading-loose">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos facere fuga nam natus omnis quas quia
+                reprehenderit voluptatem voluptatibus voluptatum! Cum eos eveniet facere maxime, molestiae praesentium
+                tempore! Aut culpa inventore iste minima provident rem repudiandae vitae? Et optio, perferendis.
+            </div>
+            <div class="bg-white rounded-md space-y-4 p-5 font-semibold">
+                <div class="  text-lg text-red-600">Come before 21st Feb</div>
+                <div class="  text-2xl underline text-gray-600 ">
+                    One Step Towards Your suscess
+                </div>
+                <div class="text-xs text-gray-600 ">
+                    For More Details, you may cal us at 1987654320. Our Support team will feel happy to help you at every stage.
+                </div>
+                <button class=" bg-red-600 px-6 py-2 text-white uppercase rounded-br-2xl">
+                    Read More.
+                </button>
+
+            </div>
+        </div>
+    </div>
+    </div>
+</div>
